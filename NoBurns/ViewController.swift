54132a5dbbfed88a37ae7026ab1b12ed80d9c5d6
Armin Spahic <arminspahic@armins-macbook-pro.local>
2018-05-27 20:17:49 +0200

    Initial Commit

---
+++ NoBurns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoBurns
+//
+//  Created by Armin Spahic on 27/05/2018.
+//  Copyright © 2018 Armin Spahic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
